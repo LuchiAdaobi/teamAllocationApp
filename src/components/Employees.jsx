630ef5c5fff2c1e5695a612963fead1f750f68dc
Luchi <luchichronicles@gmail.com>
2024-01-02 13:46:24 +0100

    create employee component, create footer component

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,102 +1,65 @@
 import { useState } from "react";
+import femaleProfile from ".././assets/female.jpg";
+import maleProfile from ".././assets/male.jpg";
 
-const Employees = () => {
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      fullName: "Bob Jones",
-      designation: "JavaScript Developer",
-      gender: "male",
-      teamName: "TeamA",
-    },
-    {
-      id: 2,
-      fullName: "Jill Bailey",
-      designation: "Node Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 3,
-      fullName: "Gail Shepherd",
-      designation: "Java Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 4,
-      fullName: "Sam Reynolds",
-      designation: "React Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 5,
-      fullName: "David Henry",
-      designation: "DotNet Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 6,
-      fullName: "Sarah Blake",
-      designation: "SQL Server DBA",
-      gender: "female",
-      teamName: "TeamB",
-    },
-    {
-      id: 7,
-      fullName: "James Bennet",
-      designation: "Angular Developer",
-      gender: "male",
-      teamName: "TeamC",
-    },
-    {
-      id: 8,
-      fullName: "Jessica Faye",
-      designation: "API Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 9,
-      fullName: "Lita Stone",
-      designation: "C++ Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 10,
-      fullName: "Daniel Young",
-      designation: "Python Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 11,
-      fullName: "Adrian Jacobs",
-      designation: "Vue Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 12,
-      fullName: "Devin Monroe",
-      designation: "Graphic Designer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-  ]);
-  console.log(employees[0])
-
+const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}) => {
   return (
-    <main>
-      <h1>Employees</h1>
-      {
-        employees.map(em => (
-            <p key={em.id}>{em.fullName}</p>
-        ))
-      }
+    <main className="container">
+      <div className="row justify-content-center mt-3 mb-3 ">
+        <div className="col-6">
+          <select
+            className="form-select form-select-lg"
+            value={selectedTeam}
+            onChange={handleTeamSelectionChange}
+          >
+            <option value="TeamA">TeamA</option>
+            <option value="TeamB">TeamB</option>
+            <option value="TeamC">TeamC</option>
+            <option value="TeamD">TeamD</option>
+          </select>
+        </div>
+        <div className="row justify-content-center mt-3 mb-3 ">
+          <div className="col-8">
+            <div className="card-collection">
+              {employees.map((em) => (
+                <div
+                key={em.id}
+                  id={em.id}
+                  className={
+                    em.teamName === selectedTeam
+                      ? "card m-2 standout"
+                      : "card m-2"
+                  }
+                  onClick={handleEmployeeCardClick}
+                >
+                  {em.gender === "male" ? (
+                    <img
+                      src={maleProfile}
+                      alt="male employee 
+                    profile"
+                      className="card-img-top"
+                    />
+                  ) : (
+                    <img
+                      src={femaleProfile}
+                      alt="female employee
+                    profile"
+                      className="card-img-top"
+                    />
+                  )}
+                  <div className="card-body">
+                    <h5 className="card-title">Full Name: {em.fullName}</h5>
+                    <p className="card-text">
+                      <b>Designation: </b>
+                      {em.designation}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
 const Footer = () => {
+  let today = new Date();
+
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h5>
+            {" "}
+            <small> Team Member Allocation App - {today.getFullYear()} </small>
+          </h5>
+        </div>
+      </div>
     </footer>
   );
 };
 
-export default Footer
+export default Footer;
